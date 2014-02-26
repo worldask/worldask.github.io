4607592ce79d2772530ee1e7231cf3ff33cb85d5
panht <panht@fzhcl.com>
2014-02-26 16:05:40 +0800

    add weiyi site

---
+++ weiyi/js/main.js
@@ -0,0 +1,71 @@
+// 当前页号
+var page = 1;
+// 每页几条数据
+var perPage = 5;
+// 主json对象
+var objData = {};
+// 距底部几像素刷新
+var scrollPixles = 20;
+
+$(function() {
+    // 首次载入
+    $.ajax({url: "data/main.json", 
+        success: function(data) {
+            objData = $.parseJSON(data);
+            next();
+
+            $("#container").on("click", "img", function(event) {
+                event.preventDefault();
+
+                // 载入作品图片
+                var json = $(this).attr("json");
+                if (json !== 'undefined' && json !== "") {
+                    $.ajax({url: "data/json/" + json,
+                        success: function(data) {
+                            $("#backdrop").removeClass("dn");
+                            $("#popContainer").removeClass("dn");
+                            var popData = $.parseJSON(data);
+
+                            $.each(popData, function(i, v) {
+                                $("#pop").append("<img src='data/images/" + v + "' />");
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    // 绑定下拉事件
+    $(window).bind("scroll", function(){
+        if( $(document).scrollTop() + $(window).height() > $(document).height() - scrollPixles){
+            next();
+        }
+    });
+
+    // 点击背景层关闭弹出层
+    $(document).on("click", function(e) {
+        if ($(e.target).closest("#pop").length > 0) {
+        } else {
+            closePopContainer();
+        }
+    });
+});
+
+// 关闭弹出层
+var closePopContainer = function() {
+    $("#backdrop").addClass("dn");
+    $("#popContainer").addClass("dn");
+    $("#pop").html("");
+};
+
+// 翻页
+var next = function() {
+    var i = 0;
+    i = (page - 1) * perPage;
+    
+    for (var j = 0; i < objData.length && j < perPage; i++, j++) {
+        $("#container").append("<img src='data/images/" + objData[i].cover + "' json='" + objData[i].json + "'/>");
+    }
+    page++;
+};
