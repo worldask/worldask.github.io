058fc2ce88e33425c9a096039939413fb3e4b683
panht <panht@fzhcl.com>
2014-02-27 12:54:30 +0800

    关闭弹出层时也关闭iosoverlay

--- weiyi/js/main.js
+++ weiyi/js/main.js
@@ -89,6 +89,7 @@ var pop = function(json) {
 // 关闭弹出层
 var closePopContainer = function() {
     $("#popContainer").stop();
+    hideIosNotify();
     $("#popContainer").fadeOut(200, function() {
         $("#backdrop").addClass("dn");
         $("#popContainer").addClass("dn");
